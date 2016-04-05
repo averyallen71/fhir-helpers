1d49d1e2a1c15135a2978a313f9dcd894713e54b
Allen <rallen35@gatech.edu>
2016-04-04 20:43:50 -0400

    adds resource property to bundle

--- helper_methods/bundle.js
+++ helper_methods/bundle.js
@@ -1,9 +1,11 @@
 module.exports = function (data) {
   if(typeof data !== "object") throw new Error('Params must be object')
   return {
-    resourceType: 'Bundle',
-    type: data.type,
-	total: data.total,
-	entry: data.entry
+	  resource: {
+		resourceType: 'Bundle',
+		type: data.type,
+		total: data.total,
+		entry: data.entry
+	  }
   }
 }
